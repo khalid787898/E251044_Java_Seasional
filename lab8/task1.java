91832ec6c661a019125dbaa6ca961f94ffb5ab83
khalid787898 <khaledalmahmud77@gmail.com>
2025-12-10 00:04:05 +0600

    Create task1.java

---
+++ lab8/task1.java
@@ -0,0 +1,39 @@
+
+public class AnimalTask {
+    public static void main(String[] args) {
+
+        Animal a = new Animal();
+        Dog d = new Dog();
+        Cat c = new Cat();
+
+        System.out.println("Animal Sound:");
+        a.sound();
+
+        System.out.println("\nDog Sound:");
+        d.sound();
+
+        System.out.println("\nCat Sound:");
+        c.sound();
+    }
+}
+
+class Animal {
+    void sound() {
+        System.out.println("Some generic sound");
+    }
+}
+
+
+class Dog extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Dog barks");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void sound() {
+        System.out.println("Cat meows");
+    }
+}
