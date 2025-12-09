8b4adf6755f84b37bc3c5102d4aedccd55a18257
khalid787898 <khaledalmahmud77@gmail.com>
2025-12-09 23:29:23 +0600

    Create task2.java

---
+++ lab1/task2.java
@@ -0,0 +1,10 @@
+
+public class MyInfo {
+    public static void main(String[] args) {
+        System.out.println("Name      : Khalid Al Mahmud");
+        System.out.println("Student ID: CCE2025-001");
+        System.out.println("Age       : 21");
+        System.out.println("Email     : khalid@example.com");
+        System.out.println("Phone     : 017xxxxxxxx");
+        System.out.println("Address   : Chittagong, Bangladesh");}
+}
